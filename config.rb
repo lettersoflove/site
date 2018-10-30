e6a0184ba4ddb4edef9e5089a2f19d3b897a89c8
tom vaillant <chroniclr@hushmail.com>
2018-10-30 22:32:14 +0100

    removed external pipeline

--- config.rb
+++ config.rb
@@ -21,8 +21,8 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
 
-activate :external_pipeline,
-  name: :webpack,
-  command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
-  source: ".tmp/dist",
-  latency: 1
+# activate :external_pipeline,
+#   name: :webpack,
+#   command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
+#   source: ".tmp/dist",
+#   latency: 1
