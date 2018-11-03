b11c51e4fda9b136ee84a207ac71ad316aa82419
tom vaillant <chroniclr@hushmail.com>
2018-11-03 17:51:22 +0100

    removed additional

--- config.rb
+++ config.rb
@@ -21,10 +21,10 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
 
-activate :external_pipeline,
-         name: :webpack,
-         command: build? ?
-         "./node_modules/webpack/bin/webpack.js --bail -p" :
-         "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
-         source: ".tmp/dist",
-         latency: 1
+# activate :external_pipeline,
+#          name: :webpack,
+#          command: build? ?
+#          "./node_modules/webpack/bin/webpack.js --bail -p" :
+#          "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+#          source: ".tmp/dist",
+#          latency: 1
