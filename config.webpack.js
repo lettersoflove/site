456a0dfb2051788ebd747e02503403f44da0e264
tom vaillant <chroniclr@hushmail.com>
2018-11-12 22:47:03 +0100

    found the bug source, updated bundles

--- config.webpack.js
+++ config.webpack.js
@@ -8,52 +8,18 @@ module.exports = {
     filename: "application.js"
     // publicPath: "/assets/"
   },
-  // module: {
-  //        rules: [
-  //            {
-  //                test: /\.js$/,
-  //                loader: 'babel-loader',
-  //                query: {
-  //                    presets: ['es2015']
-  //                }
-  //            }
-  //        ]
-  //       }
-}
-
- /* module: {
+  module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, "app")
-        ],
-        exclude: [
-          path.resolve(__dirname, "app/demo-files")
-        ],
-        issuer: { test, include, exclude },
-        enforce: "pre",
-        enforce: "post",
-        loader: "babel-loader",
-        options: {
-          presets: ["es2015"]
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          "htmllint-loader",
-          {
-            loader: "html-loader",
-            options: {
-            }
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
-        ]
+        }
       }
-    ],
-  },
-
-  plugins: [
-    // ...
-  ]
-  */
+    ]
+  }
+}
